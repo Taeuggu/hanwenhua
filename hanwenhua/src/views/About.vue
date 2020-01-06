a909b93c322de7942c1627b14a35684fbddcb669
Taeuggu <654513862@qq.com>
2020-01-06 17:10:42 +0800

    1.创建项目

---
+++ hanwenhua/src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p class='red'>qqqqqqqq</p>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+.red{
+  color:red;
+  font-size: 20px;
+}
+</style>
